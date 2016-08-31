5703dd0a69da557c731303eafa570008805ffefd
Thomas Wilburn <news@thomaswilburn.net>
2016-08-31 13:58:54 -0700

    Safari fix

--- src/js/main.js
+++ src/js/main.js
@@ -7,13 +7,4 @@ var element = document.querySelector("leaflet-map");
 var L = element.leaflet;
 var map = element.map;
 
-//ICH code for popup template if needed----------
-// var ich = require("icanhaz");
-// var templateFile = require("./_popup.html");
-// ich.addTemplate("popup", templateFile);
-
-// var onEachFeature = function(feature, layer) {
-//   layer.bindPopup(ich.popup(feature.properties))
-// };
-
  map.scrollWheelZoom.disable();
